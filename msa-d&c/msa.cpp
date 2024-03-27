d7b2582db8e18e3df29d6478502ef1cca6af80d3
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-03-27 22:44:48 +0500

    kadane

--- msa-d&c/msa.cpp
+++ msa-d&c/msa.cpp
@@ -39,11 +39,24 @@ int msaSum(int arr[], int left, int right) {
   
     int mid = (left + right) / 2; 
     return max(msaSum(arr, left, mid - 1), msaSum(arr, mid + 1, right), maxCrossingSum(arr, left, mid, right)); 
-} 
+}
+
+int msaKadane(int arr[], int size) {
+    int prevLocalMax = 0, globalMax = INT_MIN;
+
+    for (int i = 0; i < size; i++) {
+        prevLocalMax = max(arr[i], prevLocalMax + arr[i]);
+        globalMax = max(prevLocalMax, globalMax);
+    }
+
+    return globalMax;
+}
   
 int main() { 
     int arr[] = {2, 3, 4, 5, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
     cout << "Maximum contiguous sum is " << msaSum(arr, 0, size - 1) << endl;
+    cout << "Maximum contiguous sum is " << msaKadane(arr, size) << endl;
+    
     return 0; 
 }
