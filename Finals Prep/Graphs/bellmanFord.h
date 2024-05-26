e05f516c4227e0f6169f1427ada41ce007ede74f
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-05-26 15:31:24 +0500

    BF negative cycle detection/marking explanation

--- Finals Prep/Graphs/bellmanFord.h
+++ Finals Prep/Graphs/bellmanFord.h
@@ -22,12 +22,16 @@ vector<int> bellmanFordAdjMatrix(vector<vector<int>> &g, int origin) {
                 if (g[j][k] != INT_MAX and dist[j] + g[j][k] < dist[k]) // if the edge exists and the new path is shorter
                     dist[k] = dist[j] + g[j][k]; // update the distance
 
-    //check for negative cycles
+    //check if a negative cycle *exists*
     for (int j = 0; j < v; j++)
         for (int k = 0; k < v; k++)
             if (g[j][k] != INT_MAX and dist[j] + g[j][k] < dist[k]) // if a new, shorter path is found
-                return {}; // a negative cycle exists
+                return {}; // negative cycle detected
 
+    /*
+        Not that if we want to detect WHAT nodes are in the negative cycle, we can run the
+        entire algorithm again and if any distance is updated, mark it as negative infinity
+    */
 
     return dist;
 }
@@ -47,15 +51,20 @@ vector<int> bellmanFordAdjList(vector<vector<Edge>> &graph, int origin) {
         for (vector<Edge> &u : graph) // for each vertex' adjacencey list in the graph
             for (Edge &e : u) // for each outgoing edge 'e' in that adjacency list of vertex 'u'
                 if (dist[e.src] != INT_MAX and dist[e.src] + e.weight < dist[e.dest]) // if the vertex has been reached by now and the new path is shorter
-                    dist[e.dest] = dist[e.src] + e.weight; // update the 
+                    dist[e.dest] = dist[e.src] + e.weight; // update the distance
                     
-    // Re-run the algorithm and if any distance is updated, then a negative cycle exists
-    for (int i = 0; i < V-1; i++)
-        for (vector<Edge> &u : graph)
-            for (Edge &e : u)
-                if (dist[e.src] != INT_MAX and dist[e.src] + e.weight < dist[e.dest])
-                    return {}; // negative cycle detected
+    //check if a negative cycle *exists*
+    for (vector<Edge> &u : graph)
+        for (Edge &e : u)
+            if (dist[e.src] != INT_MAX and dist[e.src] + e.weight < dist[e.dest])
+                return {}; // negative cycle detected
     
+    /*
+        Not that if we want to detect WHAT nodes are in the negative cycle, rather than simply
+        confirming its existence, we can run the entire algorithm again and if any distance is 
+        updated, mark it as negative infinity rather than (dist[e.src] + e.weight)
+    */
+
     return dist;
 }
 
