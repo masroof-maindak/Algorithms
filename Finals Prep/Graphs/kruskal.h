e95495817351e4a766a4ba2b16d4aeca38533215
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-05-26 17:43:08 +0500

    fuck kruskal

--- Finals Prep/Graphs/kruskal.h
+++ Finals Prep/Graphs/kruskal.h
@@ -1,4 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
+struct Edge {
+    int from, to, weight;
+    bool operator > (const Edge &e) const {
+        return weight > e.weight;
+    }
+};
+
+int kruskal (int V, vector<Edge> &edges) {
+    // Sort edges by weight
+    int E = edges.size();
+    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+    for (Edge e : edges)
+        pq.push(e);
+    
+}
