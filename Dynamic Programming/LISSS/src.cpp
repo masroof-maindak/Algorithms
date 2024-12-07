99b007569c10a71a1d716fe789eb8e5fbc21399d
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-12-07 23:10:25 +0500

    feat: LDIS

--- Dynamic Programming/LISSS/src.cpp
+++ Dynamic Programming/LISSS/src.cpp
@@ -3,29 +3,43 @@
 
 using namespace std;
 
-int LISSS(vector<int> a) {
+int lengthOfLIS(vector<int> a) {
 	int n = a.size(), ret = 1;
 	vector<int> lisss(n, 1);
 
+	if (n < 0)
+		return 0;
+
 	/* No reason to do it in reverse, this just popped into my head first */
-
-	/* start from n-1 because lisss[n] is trivially 1 */
-	for (int i = n - 1; i >= 0; i--) {
-
-		/* start from the end and check every element until i */
+	for (int i = n - 2; i >= 0; i--) {
 		for (int j = n - 1; j > i; j--)
-			/* iff a[i] < a[j] (i.e increasing subsequence) */
 			if (a[i] < a[j])
 				lisss[i] = max(lisss[i], 1 + lisss[j]);
-
-		/* update global max */
 		ret = max(ret, lisss[i]);
 	}
 
 	return ret;
 }
 
+int lengthOfLDIS(vector<int> a) {
+	int n = a.size(), ret = 1;
+	vector<int> ldiss(n, 1);
+
+	if (n == 0)
+		return 0;
+
+	for (int i = n - 2; i >= 0; i--) {
+		for (int j = n - 1; j > i; j--)
+			if (2 * a[i] < a[j])
+				ldiss[i] = max(ldiss[i], 1 + ldiss[j]);
+		ret = max(ret, ldiss[i]);
+	}
+
+	return ret;
+}
+
 int main() {
-	cout << LISSS({10, 22, 9, 33, 21, 50, 41, 60, 80}) << "\n";
+	cout << lengthOfLIS({10, 22, 9, 33, 21, 50, 41, 60, 80}) << "\n";
+	cout << lengthOfLDIS({10, 22, 9, 45, 21, 50, 41, 60, 91}) << "\n";
 	return 0;
 }
