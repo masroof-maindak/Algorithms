3bef754fb2b5c6348ed4096354ca24b5c5240e95
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-12-13 02:19:55 +0500

    fix: variable names in rod-cutting

--- Dynamic Programming/Rod Cutting/src.cpp
+++ Dynamic Programming/Rod Cutting/src.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int rodCutDP (vector<int> prices) {
-    int n = prices.size();
-    vector<int> bestPrices (n + 1, 0);
+int rodCutDP(vector<int> prices) {
+	int n = prices.size();
+	vector<int> bestPrices(n + 1, 0);
 
-    /* bestPrices[i] = best price for a rod of length i */
-    /* price @ length 0 = trivially 0 */
+	/* bestPrices[i] = best price for a rod of length i */
+	/* price @ length 0 = trivially 0 */
 
-    for (int i = 1; i <= n; i++) {
-        int maxPrice = 0;
-        for (int j = 0; j < i; j++) {
-            int priceOfCut = bestPrices[i - j - 1];
-            int priceOfRemainder = prices[j];
-            int price = priceOfCut + priceOfRemainder;
-            maxPrice = max(maxPrice, price);
-        }
-        bestPrices[i] = maxPrice;
-    }
+	/* for all lengths */
+	for (int i = 1; i <= n; i++) {
+		int x = 0;
 
-    return bestPrices[n];
+		/* for all divisions within those lengths */
+		for (int j = 0; j < i; j++) {
+			int cutsPrice		= prices[j];
+			int remainderDecomp = bestPrices[i - j - 1];
+			x					= max(x, remainderDecomp + cutsPrice);
+		}
+
+		bestPrices[i] = x;
+	}
+
+	return bestPrices[n];
 }
 
-int main () {
-    cout << rodCutDP({1, 5, 8, 9}) << "\n";
-    return 0;
+int main() {
+	cout << rodCutDP({1, 5, 8, 9}) << "\n";
+	return 0;
 }
